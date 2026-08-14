efa02ce50f069c5574e247d6a2dae590b884faff
Maria Suvoriniva <maria.suvorinova@yandex.ru>
2026-08-14 21:26:18 +0300

    Fix remaining review comments and update Allure

--- tests/test_order_list.py
+++ tests/test_order_list.py
@@ -6,16 +6,19 @@ class TestOrderList:
 
     @allure.title("Получение списка заказов")
     @allure.description(
-        "Проверка получения списка заказов"
+        "Проверка получения списка заказов "
+        "при наличии созданного заказа"
     )
-    def test_get_orders_list_success(self, order_api):
-        """Проверяет успешное получение списка заказов."""
+    def test_get_orders_list_success(
+        self,
+        order_api,
+        create_test_order
+    ):
+        """Проверяет получение списка заказов."""
 
         response = order_api.get_orders_list()
 
         orders = response.json()["orders"]
 
-        assert (
-            response.status_code == 200
-            and isinstance(orders, list)
-        )
+        assert response.status_code == 200
+        assert isinstance(orders, list)
